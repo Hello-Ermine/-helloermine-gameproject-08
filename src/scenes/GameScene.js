c8a833aaaee1edeabcff768df8d71f40951b1482
Boss <c3bosskung@gmail.com>
2021-11-13 02:40:06 +0700

    edit p1

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -7,7 +7,7 @@ let player1;
 let player2;
 
 let timeShuriken = 0;
-let delayShuriken = 1000;
+let delayShuriken = 400;
 
 let shuriken;
 let shurikenGroupP1;
@@ -85,7 +85,7 @@ class GameScene extends Phaser.Scene {
             key: 'player1AniRight',
             frames: this.anims.generateFrameNumbers('player1R', {
                 start: 1,
-                end: 5
+                end: 4
             }),
             duration: 500,
             repeat: -1
@@ -97,7 +97,7 @@ class GameScene extends Phaser.Scene {
                 start: 0,
                 end: 2
             }),
-            duration: 650,
+            duration: 400,
             framerate: 1,
             repeat: 10,
             callbackScope: this,
@@ -109,7 +109,7 @@ class GameScene extends Phaser.Scene {
                 start: 0,
                 end: 0
             }),
-            duration: 100,
+            duration: 400,
             delay: 1000,
             repeat: -1
         })
@@ -125,32 +125,9 @@ class GameScene extends Phaser.Scene {
             repeat: -1
         })
 
-        //-------------------------------------
-
-        player2 = this.physics.add.sprite(800, 800, 'player2L').setScale(0.5);
         this.anims.create({
-            key: 'player2AniLeft',
-            frames: this.anims.generateFrameNumbers('player2L', {
-                start: 0,
-                end: 4
-            }),
-            duration: 500,
-            repeat: -1
-        })
-
-        this.anims.create({
-            key: 'player2AniRight',
-            frames: this.anims.generateFrameNumbers('player2R', {
-                start: 0,
-                end: 4
-            }),
-            duration: 500,
-            repeat: -1
-        })
-
-        this.anims.create({
-            key: 'player2AniDown',
-            frames: this.anims.generateFrameNumbers('player2R', {
+            key: 'player1AniUpL',
+            frames: this.anims.generateFrameNumbers('player1L', {
                 start: 1,
                 end: 1
             }),
@@ -158,15 +135,50 @@ class GameScene extends Phaser.Scene {
             repeat: -1
         })
 
-        this.anims.create({
-            key: 'player2AniUp',
-            frames: this.anims.generateFrameNumbers('player2R', {
-                start: 4,
-                end: 4
-            }),
-            duration: 500,
-            repeat: -1
-        })
+        //-------------------------------------
+
+        // player2 = this.physics.add.sprite(800, 800, 'player2L').setScale(0.5);
+        // this.anims.create({
+        //     key: 'player2AniLeft',
+        //     frames: this.anims.generateFrameNumbers('player2L', {
+        //         start: 0,
+        //         end: 0
+        //     }),
+        //     duration: 500,
+        //     repeat: -1
+        // })
+
+        // this.anims.create({
+        //     key: 'player2AniRight',
+        //     frames: this.anims.generateFrameNumbers('player2R', {
+        //         start: 0,
+        //         end: 3
+        //     }),
+        //     duration: 500,
+        //     repeat: -1
+        // })
+
+        // this.anims.create({
+        //     key: 'player2AniDown',
+        //     frames: this.anims.generateFrameNumbers('player2R', {
+        //         start: 1,
+        //         end: 1
+        //     }),
+        //     duration: 500,
+        //     repeat: -1
+        // })
+
+        // this.anims.create({
+        //     key: 'player2AniUp',
+        //     frames: this.anims.generateFrameNumbers('player2R', {
+        //         start: 4,
+        //         end: 4
+        //     }),
+        //     duration: 500,
+        //     repeat: -1
+        // })
+
+
 
         this.anims.create({
             key: 'shurikenAni',
@@ -188,7 +200,7 @@ class GameScene extends Phaser.Scene {
         shurikenGroupP2 = this.physics.add.group();
 
         player1.setCollideWorldBounds(true)
-        player2.setCollideWorldBounds(true)
+            //player2.setCollideWorldBounds(true)
 
         cursor = this.input.keyboard.createCursorKeys();
         keyM = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.M);
@@ -204,46 +216,46 @@ class GameScene extends Phaser.Scene {
     }
 
     update(delta, time) {
-        this.physics.add.overlap(shurikenGroupP1R, player2, (S, R) => {
-            R.destroy();
-            player2HealthBar--;
-            if (player2HealthBar <= 0) {
-                console.log('Player1 Win');
-            }
-            console.log('Health Player2 : ' + player2HealthBar);
-        })
+        // this.physics.add.overlap(shurikenGroupP1R, player2, (S, R) => {
+        //     R.destroy();
+        //     player2HealthBar--;
+        //     if (player2HealthBar <= 0) {
+        //         console.log('Player1 Win');
+        //     }
+        //     console.log('Health Player2 : ' + player2HealthBar);
+        // })
 
-        this.physics.add.overlap(shurikenGroupP1L, player2, (S, R) => {
-            R.destroy();
-            player2HealthBar--;
-            if (player2HealthBar <= 0) {
-                console.log('Player1 Win');
-            }
-            console.log('Health Player2 : ' + player2HealthBar);
-        })
+        // this.physics.add.overlap(shurikenGroupP1L, player2, (S, R) => {
+        //     R.destroy();
+        //     player2HealthBar--;
+        //     if (player2HealthBar <= 0) {
+        //         console.log('Player1 Win');
+        //     }
+        //     console.log('Health Player2 : ' + player2HealthBar);
+        // })
 
-        this.physics.add.overlap(shurikenGroupP2R, player1, (S, R) => {
-            R.destroy();
-            player1HealthBar--;
-            if (player1HealthBar <= 0) {
-                console.log('Player2 Win');
-            }
-            console.log('Health Player1 : ' + player1HealthBar);
-        })
+        // this.physics.add.overlap(shurikenGroupP2R, player1, (S, R) => {
+        //     R.destroy();
+        //     player1HealthBar--;
+        //     if (player1HealthBar <= 0) {
+        //         console.log('Player2 Win');
+        //     }
+        //     console.log('Health Player1 : ' + player1HealthBar);
+        // })
 
-        this.physics.add.overlap(shurikenGroupP2L, player1, (S, R) => {
-            R.destroy();
-            player1HealthBar--;
-            if (player1HealthBar <= 0) {
-                console.log('Player2 Win');
-            }
-            console.log('Health Player1 : ' + player1HealthBar);
-        })
+        // this.physics.add.overlap(shurikenGroupP2L, player1, (S, R) => {
+        //     R.destroy();
+        //     player1HealthBar--;
+        //     if (player1HealthBar <= 0) {
+        //         console.log('Player2 Win');
+        //     }
+        //     console.log('Health Player1 : ' + player1HealthBar);
+        // })
 
-        this.physics.add.overlap(shurikenGroupP1, shurikenGroupP2, (S, R) => {
-            R.destroy();
-            S.destroy();
-        })
+        // this.physics.add.overlap(shurikenGroupP1, shurikenGroupP2, (S, R) => {
+        //     R.destroy();
+        //     S.destroy();
+        // })
 
         //player1
         if (test == 0) {
@@ -276,120 +288,137 @@ class GameScene extends Phaser.Scene {
                 // }
                 directionP1 = 'right'
             } else {
-                player1.anims.play('player1AniUp', true);
+                if (directionP1 === 'right') {
+                    player1.anims.play('player1AniUp', true);
+                } else {
+                    player1.anims.play('player1AniUpL', true);
+                }
                 player1.setVelocityX(0);
             }
         }
+
         if (keySpace.isDown && delta >= (timeShuriken + delayShuriken)) {
             test = 1;
-            player1.anims.play('player1AniShootR', true);
-            this.time.addEvent({
-                delay: 300,
-                callback: function() {
-                    player1.anims.play('player1AniUp', true);
-                    test = 0;
-                },
-                callbackScope: this,
-                loop: false,
-            });
+            if (directionP1 === 'right') {
+                player1.anims.play('player1AniShootR', true);
+                this.time.addEvent({
+                    delay: 200,
+                    callback: function() {
+                        player1.anims.play('player1AniUp', true);
+                        test = 0;
+                    },
+                    callbackScope: this,
+                    loop: false,
+                });
+            } else {
+                player1.anims.play('player1AniShootL', true);
+                this.time.addEvent({
+                    delay: 200,
+                    callback: function() {
+                        player1.anims.play('player1AniUpL', true);
+                        test = 0;
+                    },
+                    callbackScope: this,
+                    loop: false,
+                });
+            }
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(keySpace) && delta >= (timeShuriken + delayShuriken)) {
+            if (directionP1 === 'left') {
+                shuriken = this.physics.add.sprite(player1.x, player1.y, 'shuriken');
+                shuriken.setSize(220, 220);
+                shuriken.anims.play('shurikenAni', true);
+                shuriken.setScale(0.2);
+                shurikenGroupP1L.add(shuriken);
+                shurikenGroupP1L.setVelocityX(-400);
+                // bulletL = this.physics.add.image(player1.x - 100, player1.y, 'bullet')
+                // .setScale(0.1);
+                // bulletL.setVelocityX(-800);
+            } else {
+                shuriken = this.physics.add.sprite(player1.x, player1.y, 'shuriken');
+                shuriken.setSize(220, 220);
+                shuriken.anims.play('shurikenAni', true);
+                shuriken.setScale(0.2);
+                shurikenGroupP1R.add(shuriken);
+                shurikenGroupP1R.setVelocityX(400);
+                // bulletR = this.physics.add.image(player1.x + 100, player1.y, 'bulletR')
+                // .setScale(0.1);
+                // bulletR.setVelocityX(800);
+            }
             timeShuriken = delta;
         }
 
-        // if (Phaser.Input.Keyboard.JustDown(keySpace) && delta >= (timeShuriken + delayShuriken)) {
-        //     if (directionP1 === 'left') {
-        //         shuriken = this.physics.add.sprite(player1.x, player1.y, 'shuriken');
+        //player2
+        // if (cursor.up.isDown) {
+        //     player2.setVelocityY(-500);
+        //     player2.anims.play('player2AniUp', true);
+        // } else if (cursor.down.isDown) {
+        //     player2.setVelocityY(500);
+        //     player2.anims.play('player2AniDown', true);
+        // } else {
+        //     player2.setVelocityY(0);
+        // }
+        // if (cursor.left.isDown) {
+        //     player2.setVelocityX(-500);
+        //     player2.anims.play('player2AniLeft', true);
+        //     directionP2 = 'left';
+        // } else if (cursor.right.isDown) {
+        //     player2.setVelocityX(500);
+        //     player2.anims.play('player2AniRight', true);
+        //     directionP2 = 'right';
+        // } else {
+        //     player2.setVelocityX(0);
+        //     player2.anims.play('player2AniUp', true);
+        // }
+        // if (Phaser.Input.Keyboard.JustDown(keyM)) {
+        //     if (directionP2 === 'left') {
+        //         shuriken = this.physics.add.sprite(player2.x, player2.y, 'shuriken');
         //         shuriken.setSize(220, 220);
         //         shuriken.anims.play('shurikenAni', true);
         //         shuriken.setScale(0.2);
-        //         shurikenGroupP1L.add(shuriken);
-        //         shurikenGroupP1L.setVelocityX(-400);
-        //         // bulletL = this.physics.add.image(player1.x - 100, player1.y, 'bullet')
-        //         // .setScale(0.1);
-        //         // bulletL.setVelocityX(-800);
+        //         shurikenGroupP2L.add(shuriken);
+        //         shurikenGroupP2L.setVelocityX(-400);
         //     } else {
-        //         shuriken = this.physics.add.sprite(player1.x, player1.y, 'shuriken');
+        //         shuriken = this.physics.add.sprite(player2.x, player2.y, 'shuriken');
         //         shuriken.setSize(220, 220);
         //         shuriken.anims.play('shurikenAni', true);
         //         shuriken.setScale(0.2);
-        //         shurikenGroupP1R.add(shuriken);
-        //         shurikenGroupP1R.setVelocityX(400);
-        //         // bulletR = this.physics.add.image(player1.x + 100, player1.y, 'bulletR')
-        //         // .setScale(0.1);
-        //         // bulletR.setVelocityX(800);
+        //         shurikenGroupP2R.add(shuriken);
+        //         shurikenGroupP2R.setVelocityX(400);
         //     }
-        //     timeShuriken = delta;
+        // }
+        // for (let i = 0; i < shurikenGroupP1R.getChildren().length; i++) {
+        //     if (shurikenGroupP1R.getChildren()[i].x < 0) {
+        //         shurikenGroupP1R.getChildren()[i].destroy();
+        //     } else if (shurikenGroupP1R.getChildren()[i].x > 1280) {
+        //         shurikenGroupP1R.getChildren()[i].destroy();
+        //     }
         // }
 
-        //player2
-        if (cursor.up.isDown) {
-            player2.setVelocityY(-500);
-            player2.anims.play('player2AniUp', true);
-        } else if (cursor.down.isDown) {
-            player2.setVelocityY(500);
-            player2.anims.play('player2AniDown', true);
-        } else {
-            player2.setVelocityY(0);
-        }
-        if (cursor.left.isDown) {
-            player2.setVelocityX(-500);
-            player2.anims.play('player2AniLeft', true);
-            directionP2 = 'left';
-        } else if (cursor.right.isDown) {
-            player2.setVelocityX(500);
-            player2.anims.play('player2AniRight', true);
-            directionP2 = 'right';
-        } else {
-            player2.setVelocityX(0);
-            player2.anims.play('player2AniUp', true);
-        }
-        if (Phaser.Input.Keyboard.JustDown(keyM)) {
-            if (directionP2 === 'left') {
-                shuriken = this.physics.add.sprite(player2.x, player2.y, 'shuriken');
-                shuriken.setSize(220, 220);
-                shuriken.anims.play('shurikenAni', true);
-                shuriken.setScale(0.2);
-                shurikenGroupP2L.add(shuriken);
-                shurikenGroupP2L.setVelocityX(-400);
-            } else {
-                shuriken = this.physics.add.sprite(player2.x, player2.y, 'shuriken');
-                shuriken.setSize(220, 220);
-                shuriken.anims.play('shurikenAni', true);
-                shuriken.setScale(0.2);
-                shurikenGroupP2R.add(shuriken);
-                shurikenGroupP2R.setVelocityX(400);
-            }
-        }
-        for (let i = 0; i < shurikenGroupP1R.getChildren().length; i++) {
-            if (shurikenGroupP1R.getChildren()[i].x < 0) {
-                shurikenGroupP1R.getChildren()[i].destroy();
-            } else if (shurikenGroupP1R.getChildren()[i].x > 1280) {
-                shurikenGroupP1R.getChildren()[i].destroy();
-            }
-        }
+        // for (let j = 0; j < shurikenGroupP1L.getChildren().length; j++) {
+        //     if (shurikenGroupP1L.getChildren()[j].x < 0) {
+        //         shurikenGroupP1L.getChildren()[j].destroy();
+        //     } else if (shurikenGroupP1L.getChildren()[j].x > 1280) {
+        //         shurikenGroupP1L.getChildren()[j].destroy();
+        //     }
+        // }
 
-        for (let j = 0; j < shurikenGroupP1L.getChildren().length; j++) {
-            if (shurikenGroupP1L.getChildren()[j].x < 0) {
-                shurikenGroupP1L.getChildren()[j].destroy();
-            } else if (shurikenGroupP1L.getChildren()[j].x > 1280) {
-                shurikenGroupP1L.getChildren()[j].destroy();
-            }
-        }
+        // for (let k = 0; k < shurikenGroupP2R.getChildren().length; k++) {
+        //     if (shurikenGroupP2R.getChildren()[k].x < 0) {
+        //         shurikenGroupP2R.getChildren()[k].destroy();
+        //     } else if (shurikenGroupP2R.getChildren()[k].x > 1280) {
+        //         shurikenGroupP2R.getChildren()[k].destroy();
+        //     }
+        // }
 
-        for (let k = 0; k < shurikenGroupP2R.getChildren().length; k++) {
-            if (shurikenGroupP2R.getChildren()[k].x < 0) {
-                shurikenGroupP2R.getChildren()[k].destroy();
-            } else if (shurikenGroupP2R.getChildren()[k].x > 1280) {
-                shurikenGroupP2R.getChildren()[k].destroy();
-            }
-        }
-
-        for (let l = 0; l < shurikenGroupP2L.getChildren().length; l++) {
-            if (shurikenGroupP2L.getChildren()[l].x < 0) {
-                shurikenGroupP2L.getChildren()[l].destroy();
-            } else if (shurikenGroupP2L.getChildren()[l].x > 1280) {
-                shurikenGroupP2L.getChildren()[l].destroy();
-            }
-        }
+        // for (let l = 0; l < shurikenGroupP2L.getChildren().length; l++) {
+        //     if (shurikenGroupP2L.getChildren()[l].x < 0) {
+        //         shurikenGroupP2L.getChildren()[l].destroy();
+        //     } else if (shurikenGroupP2L.getChildren()[l].x > 1280) {
+        //         shurikenGroupP2L.getChildren()[l].destroy();
+        //     }
+        // }
 
     }
 }
