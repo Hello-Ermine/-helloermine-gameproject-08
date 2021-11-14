1d1738d2540d3ca41e8ceac05aa0f11297da8831
Boss <c3bosskung@gmail.com>
2021-11-14 16:46:27 +0700

    can export shuriken :)

--- src/scenes/test.js
+++ src/scenes/test.js
@@ -10,6 +10,7 @@ class test extends Phaser.Scene {
     }
 
     preload() {
+        //--------------------------------------------------------------------------Player1
         if (selectedShurikenP1 == 'shuriken1') {
             this.load.spritesheet('shurikenP1', 'src/image/bullet/shuriken1.png', {
                 frameWidth: 325,
@@ -26,9 +27,32 @@ class test extends Phaser.Scene {
                 frameElement: 183
             });
         }
+
+        //--------------------------------------------------------------------------//Player1
+
+        //--------------------------------------------------------------------------Player2
+        if (selectedShurikenP2 == 'shuriken1') {
+            this.load.spritesheet('shurikenP2', 'src/image/bullet/shuriken1.png', {
+                frameWidth: 325,
+                frameHeight: 332
+            });
+        } else if (selectedShurikenP2 == 'shuriken2') {
+            this.load.spritesheet('shurikenP2', 'src/image/bullet/shuriken2.png', {
+                frameWidth: 169.7,
+                frameElement: 210
+            });
+        } else {
+            this.load.spritesheet('shurikenP2', 'src/image/bullet/shuriken3.png', {
+                frameWidth: 170.125,
+                frameElement: 183
+            });
+        }
+        //--------------------------------------------------------------------------//Player2
     }
 
     create() {
+        this.physics.add.sprite(200, 200, 'shurikenP1');
+        this.physics.add.sprite(700, 200, 'shurikenP2');
 
     }
     update(delta, time) {
